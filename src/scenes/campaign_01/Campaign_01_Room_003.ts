6e70d5e23397f3cfb1923187972759460cac348b
b0wter <b0wter@gmx.de>
2020-07-17 08:59:05 +0200

    Added height tiles and layer to old maps.

---
+++ src/scenes/campaign_01/Campaign_01_Room_003.ts
@@ -0,0 +1,23 @@
+import GameplayScene from './../GameplayScene'
+import TilemapDefinition from './../TilemapDefinition'
+
+export default class Campaign_01_Room_003 extends GameplayScene
+{
+    public static readonly SceneName = "Campaign_01_Room_003"
+
+    protected get mapName() { return "campaign_01_room_003_map" }
+
+    protected get tilemapDefinitions() { return [ new TilemapDefinition("terrain", "floor", "tiles") ]}
+    protected get collisionTilemapDefinition() { return new TilemapDefinition("collision", "collision", "collision_tiles")}
+
+	constructor()
+	{
+        super(Campaign_01_Room_003.SceneName)
+    }
+    
+    protected sceneSpecificUpdate(t: number, dt: number)
+    {
+        //
+    }
+
+}
