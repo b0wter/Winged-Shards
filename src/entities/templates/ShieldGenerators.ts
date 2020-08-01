13a4489134a0f6f3fa5d7901de927b6ee2a9689d
b0wter <b0wter@gmx.de>
2020-08-01 21:49:13 +0200

    Removed redundant code.

--- src/entities/templates/ShieldGenerators.ts
+++ src/entities/templates/ShieldGenerators.ts
@@ -4,30 +4,30 @@ import { Manufacturers } from '~/utilities/Manufacturers';
 
 export class SmallShieldGenerator extends ShieldGenerator
 {
-    public readonly hardPointSize = HardPointSize.Small
-    public readonly hardPointType = HardPointType.WithoutExtras
-    public readonly manufacturer = Manufacturers.BattlePrep
-    public readonly modelName = "SSG-01a"
-    public readonly maxShields = 100
-    public readonly rechargeRate = 2
+    hardPointSize = HardPointSize.Small
+    hardPointType = HardPointType.WithoutExtras
+    manufacturer = Manufacturers.BattlePrep
+    modelName = "SSG-01a"
+    maxShields = 100
+    rechargeRate = 2
 }
 
 export class MediumShieldGenerator extends ShieldGenerator
 {
-    public readonly hardPointSize = HardPointSize.Medium
-    public readonly hardPointType = HardPointType.WithoutExtras
-    public readonly manufacturer = Manufacturers.BattlePrep
-    public readonly modelName = "SSG-01a Deluxe"
-    public readonly maxShields = 150
-    public readonly rechargeRate = 4
+    hardPointSize = HardPointSize.Medium
+    hardPointType = HardPointType.WithoutExtras
+    manufacturer = Manufacturers.BattlePrep
+    modelName = "SSG-01a Deluxe"
+    maxShields = 150
+    rechargeRate = 4
 }
 
 export class LargeShieldGenerator extends ShieldGenerator
 {
-    public readonly hardPointSize = HardPointSize.Large
-    public readonly hardPointType = HardPointType.WithoutExtras
-    public readonly manufacturer = Manufacturers.BattlePrep
-    public readonly modelName = "SSG-01a Extreme"
-    public readonly maxShields = 200
-    public readonly rechargeRate = 6
+    hardPointSize = HardPointSize.Large
+    hardPointType = HardPointType.WithoutExtras
+    manufacturer = Manufacturers.BattlePrep
+    modelName = "SSG-01a Extreme"
+    maxShields = 200
+    rechargeRate = 6
 }
