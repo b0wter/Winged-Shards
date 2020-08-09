a3222f612c2d3b6dfb102853f6609da065d08472
b0wter <b0wter@gmx.de>
2020-08-09 13:31:32 +0200

    Made DamageDealt members read-only.

--- src/entities/DamageDealt.ts
+++ src/entities/DamageDealt.ts
@@ -4,9 +4,9 @@ export default class DamageDealt
     public get dealtHullDamage() { return this.hullDamage > 0 }
     public get dealtStructureDamage() { return this.structureDamage > 0 }
 
-    constructor(public shieldDamage,
-                public hullDamage,
-                public structureDamage)
+    constructor(public readonly shieldDamage,
+                public readonly hullDamage,
+                public readonly structureDamage)
     {
         //
     }
